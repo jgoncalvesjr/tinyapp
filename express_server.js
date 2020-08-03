d4988d5f5813379600440388dd1e7ca046091fdd
jgoncalvesjr <jgoncalvesjr@gmail.com>
2020-08-03 18:56:00 +0000

    Added new link creation page

--- express_server.js
+++ express_server.js
@@ -28,6 +28,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls.json", (req, res) => {
   res.send(urlDatabase);
 });
