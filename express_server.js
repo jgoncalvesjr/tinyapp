037347855e88eee4fd64051536559b2445f20b5f
jgoncalvesjr <jgoncalvesjr@gmail.com>
2020-08-03 19:12:51 +0000

    Installed body-parser to implement form submission, implemented random string generator, organized server code

--- express_server.js
+++ express_server.js
@@ -1,17 +1,27 @@
 // Depedencies
 
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
-// Default port and templating engine setup
+// Default port, app and engine usage
 
 const PORT = 8080; 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+// URL Database
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+// Random ID Generator - generates a random a string of 6 random aplhanumeric characters
+
+const generateRandomString = () => {
+  return Math.random().toString(36).substring(2,8);
+}
 
 // Endpoints
 
@@ -26,6 +36,11 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/new", (req, res) => {
