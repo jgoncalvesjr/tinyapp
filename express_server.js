475cfe60ec88470655677af53e237a42e5450db7
jgoncalvesjr <jgoncalvesjr@gmail.com>
2020-08-04 18:42:16 +0000

    Installed cookie-parser package, configured it in server file

--- express_server.js
+++ express_server.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
 const app = express();
 const morgan = require('morgan');
 
@@ -11,6 +12,7 @@ const PORT = 8080;
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan('dev'));
+app.use(cookieParser());
 
 // URL Database
 
