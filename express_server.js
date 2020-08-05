beeca814ccd1cccaafc5da650c8b52c677b11883
jgoncalvesjr <jgoncalvesjr@gmail.com>
2020-08-05 21:20:13 +0000

    Updated permission features and logic

--- express_server.js
+++ express_server.js
@@ -47,6 +47,18 @@ const verifyEmail = email => {
   }
 };
 
+// Filters URL database belonging to that specific user
+
+const urlsForUser = id => {
+  const userData = {};
+  for (const url in urlDatabase) {
+    if (urlDatabase[url].userID === id) {
+      userData[url] = urlDatabase[url];
+    }
+  }
+  return userData;
+}
+
 // Random ID Generator - generates a random a string of 6 random aplhanumeric characters
 
 const generateRandomString = () => {
@@ -75,7 +87,9 @@ app.get("/register", (req, res) => {
 
 app.get("/login", (req, res) => {
   const user = req.cookies.userID;
-  const templateVars = { user: users[user] };
+  const templateVars = { 
+    user: users[user],
+  };
   res.render('urls_login',templateVars);
 });
 
@@ -86,9 +100,8 @@ app.get("/urls", (req, res) => {
   const user = req.cookies.userID;
   const templateVars = {
     user: users[user],
-    urls: urlDatabase
+    urls: urlsForUser(user)
   };
-  console.log(templateVars.urls)
   res.render("urls_index", templateVars);
 });
 
@@ -96,11 +109,10 @@ app.get("/urls", (req, res) => {
 
 app.get("/urls/new", (req, res) => {
   const user = req.cookies.userID;
-  const templateVars = { user: users[user] };
   if (!user) {
-    res.redirect('/login');
-    return;
+    return res.redirect('/urls');
   }
+  const templateVars = { user: users[user] };
   res.render("urls_new", templateVars);
 });
 
@@ -118,6 +130,9 @@ app.get("/urls/:id", (req, res) => {
     user: users[user],
     shortURL: req.params.id,
     longURL: urlDatabase[req.params.id].longURL };
+  if (!user) {
+    return res.redirect('/urls');
+  }
   res.render("urls_show", templateVars);
 });
 
@@ -170,23 +185,21 @@ app.post("/logout", (req, res) => {
   res.redirect('/urls');
 });
 
-// On link shortening POST, generates a random alphanumeric string for shortened link, makes it a key/pair value in urlDatabase object, and redirects to newly created shortened
+// On link shortening POST, generates a random alphanumeric string for shortened link, makes it a key/pair value in urlDatabase object, and redirects to newly created shortened page
 
 app.post("/urls", (req, res) => {
   console.log(req.body); // logs POST request body to server console. Should be the long URL
-  const user = req.cookies.userID;
+  const userID = req.cookies.userID;
   const shortURL = generateRandomString();
   if (!req.body.longURL) {
     return res.status(400).send('Web address cannot be empty!');
   }
-  urlDatabase[shortURL] = {
-    longURL: req.body.longURL,
-    UserID: users[user].id
-  }
+  urlDatabase[shortURL] = { longURL: req.body.longURL, userID };
+  console.log(urlDatabase);
   res.redirect(`/urls/${shortURL}`);
 });
 
-// Specific path (:/) routes. GET followed by POST
+// Specific path routes. GET followed by POST
 
 // Redirection from shortened URL to original URL
 
@@ -204,19 +217,32 @@ app.get("/u/:id", (req, res) => {
 
 app.post("/urls/:id", (req, res) => {
   console.log(req.body); // prints the new URL in console
-  if (!req.body.longURL) {
+  const shortURL = req.params.id;
+  const longURL = req.body.longURL;
+  const userID = req.cookies.userID;
+  const userURL = urlsForUser(userID);
+  if (!longURL) {
     return res.status(400).send('Web address cannot be empty!');
   }
-  urlDatabase[req.params.id].longURL = req.body.longURL;
-  res.redirect('/urls');
+  if (userURL.hasOwnProperty(shortURL)) {
+    urlDatabase[shortURL] = { longURL, userID };
+    return res.redirect(`/urls/${shortURL}`);
+  }
+  res.status(403).send('TinyURL does not belong to current user, permission denied!\n');
 });
 
 // deletes selected link and redirects to Index page (/urls)
 
 app.post("/urls/:id/delete", (req, res) => {
-  delete urlDatabase[req.params.id];
-  console.log(urlDatabase); // prints object without deleted URL in the console
-  res.redirect("/urls");
+  const userID = req.cookies.userID;
+  const shortURL = req.params.id;
+  const userURL = urlsForUser(userID);
+  if (userURL.hasOwnProperty(shortURL)) {
+    delete urlDatabase[shortURL];
+    console.log(urlDatabase); // prints object without deleted URL in the console
+    return res.redirect("/urls");
+  }
+  res.status(403).send('TinyURL does not belong to current user, permission denied!\n');
 });
 
 // Server listening
